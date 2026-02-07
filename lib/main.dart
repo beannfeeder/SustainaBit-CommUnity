5d94c7a8e878ad14e17f1f7fab713cce40cf81e9
jer-tan <85390088+jer-tan@users.noreply.github.com>
2026-02-07 22:44:48 +0800

    Fix flutter run issue

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,5 @@
 import 'package:flutter/material.dart';
-import 'package:provider/provider.dart';
+
 import 'src/config/app_theme.dart';
 import 'src/routes/app_router.dart';
 
@@ -12,19 +12,13 @@ class MyApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return MultiProvider(
-      providers: [
-        // Add your providers here
-        // Example: ChangeNotifierProvider(create: (_) => ThemeProvider()),
-      ],
-      child: MaterialApp.router(
-        title: 'SustainaBit CommUnity',
-        debugShowCheckedModeBanner: false,
-        theme: AppTheme.lightTheme,
-        darkTheme: AppTheme.darkTheme,
-        themeMode: ThemeMode.system,
-        routerConfig: AppRouter.router,
-      ),
+    return MaterialApp.router(
+      title: 'SustainaBit CommUnity',
+      debugShowCheckedModeBanner: false,
+      theme: AppTheme.lightTheme,
+      darkTheme: AppTheme.darkTheme,
+      themeMode: ThemeMode.system,
+      routerConfig: AppRouter.router,
     );
   }
 }
